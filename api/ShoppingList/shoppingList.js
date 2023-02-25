573141f2c3ae825df2679442fe47128d7cb34cf0
Dylan Maurits <dylan.maurits@experius.nl>
2023-02-25 17:16:34 +0100

    Added json body to http file

--- api/ShoppingList/shoppingList.js
+++ api/ShoppingList/shoppingList.js
@@ -1,5 +1,20 @@
+const db = require("../../app/db.js")
+
 async function postShoppingList(req, res) {
-    res.send('You posted a recipe!')
+
+    console.log(req.body.list.items)
+    const pool = await db()
+    let message = 'You posted a shopping list!'
+    let status = 200
+
+    // try {
+    //     await pool.query(`INSERT INTO shopping_list (string) VALUES (?)`, [string])
+    // } catch (e) {
+    //     message = e.message
+    //     status = 500
+    // }
+
+    res.status(status).send({message});
 }
 
 module.exports = postShoppingList
