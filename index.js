364a460db49fb5f4e8b8b3aa6713838a31a2697e
Dylan Maurits <dylan.maurits@experius.nl>
2023-02-25 14:55:13 +0100

    Created routing, model and phpstorm http request

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const functions = require('@google-cloud/functions-framework')
 const mysql = require('promise-mysql');
+const postRecipe = require("./models/recipes/recipe.js")
 require('dotenv').config()
 
+functions.http('httpEntry', async (req, res) => {
+    switch (req.url.split('/')[1]) {
+        case 'postRecipe': await postRecipe(req, res); break;
+        default: res.send('404')
+    }
+})
---
+++ models/recipes/recipe.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+async function postRecipe(req, res) {
+    res.send('You posted a recipe!')
+}
+
+module.exports = postRecipe
